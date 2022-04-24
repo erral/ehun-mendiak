ffe322a48fe8d51a64dec912b38b1f50cd3d305a
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-04-24 16:42:29 +0200

    use Hashrouter for GH-Pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { FrontPage } from './pages/FrontPage/FrontPage';
 import { MapPage } from './pages/MapPage/MapPage';
 
 export const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes path="/">
         <Route index element={<FrontPage />} />
         <Route exact path="/mapa" element={<MapPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
